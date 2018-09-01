3835494521065d56df1e3c6529fd61d41e6b4f84
Pamplemousse <xav.maso@gmail.com>
2018-09-01 15:36:29 +0200

    Refactoring
      * remove duplication when creating literal token from the buffer
      * use a single buffer for literal and then operator
      * `is_digit` and `is_operator` functions should not exist outside
      `tokenize`

--- main.rs
+++ main.rs
@@ -24,55 +24,53 @@ fn main() {
 }
 
 fn tokenize(expression: &str) -> Vec<Token> {
+    fn is_digit(c: char) -> bool {
+        match c {
+            '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => true,
+            _ => false
+        }
+    }
+
+    fn is_operator(c: char) -> bool {
+        match c {
+            '+' | '-' | '*' | '/' | '^' => true,
+            _ => false
+        }
+    }
+
+    fn pop_buffer_to_tokens(buffer: &mut Vec<char>, tokens: &mut Vec<Token>, kind: String) {
+        tokens.push(
+            Token { kind: kind,
+                    value: buffer.clone() }
+        );
+        buffer.clear();
+    }
+
     let mut tokens: Vec<Token> = Vec::new();
-    let mut literal_buffer: Vec<char> = Vec::new();
+    let mut buffer: Vec<char> = Vec::new();
 
     for character in expression.chars() {
         match character {
             c if is_digit(c) => {
-                literal_buffer.push(character);
+                buffer.push(character);
             },
             c if is_operator(c) => {
-                if !(literal_buffer.is_empty()) {
+                if !(buffer.is_empty()) {
                     let kind = String::from("literal");
-                    tokens.push(
-                        Token { kind: kind, value: literal_buffer.clone() }
-                    );
-                    literal_buffer.clear();
+                    pop_buffer_to_tokens(&mut buffer, &mut tokens, kind);
                 }
 
+                buffer.push(character);
                 let kind = String::from("operator");
-                let mut characters: Vec<char> = Vec::new();
-                characters.push(c);
-                tokens.push(
-                    Token { kind: kind, value: characters }
-                );
+                pop_buffer_to_tokens(&mut buffer, &mut tokens, kind);
             },
             _ => ()
         };
     }
 
-    if !(literal_buffer.is_empty()) {
-        let kind = String::from("literal");
-        tokens.push(
-            Token { kind: kind, value: literal_buffer.clone() }
-        );
-        literal_buffer.clear();
-    }
+    // A valid expression will never end with an operator
+    let kind = String::from("literal");
+    pop_buffer_to_tokens(&mut buffer, &mut tokens, kind);
 
     return tokens;
-}
-
-fn is_digit(c: char) -> bool {
-    match c {
-        '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' => true,
-        _ => false
-    }
-}
-
-fn is_operator(c: char) -> bool {
-    match c {
-        '+' | '-' | '*' | '/' | '^' => true,
-        _ => false
-    }
 }
