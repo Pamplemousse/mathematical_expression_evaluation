136a041594f15d290e0da1d4d3097688f37447f8
Pamplemousse <xav.maso@gmail.com>
2018-12-25 10:55:20 +0100

    Allow to order operators by their precedence

--- src/tokenizer/token/operator.rs
+++ src/tokenizer/token/operator.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fmt::{Debug, Display, Formatter, Error};
 
 #[derive(Clone)]
@@ -27,6 +28,14 @@ impl Operator {
         };
         return operator;
     }
+
+    fn priority(&self) -> u32 {
+        match *self {
+            Operator::Plus | Operator::Minus => 1,
+            Operator::Times | Operator::Slash => 2,
+            Operator::Caret => 3
+        }
+    }
 }
 
 impl Display for Operator {
@@ -51,6 +60,14 @@ impl Debug for Operator {
 impl PartialEq for Operator {
     fn eq(&self, other: &Operator) -> bool {
         self.to_char() == other.to_char()
+    }
+}
+
+impl PartialOrd for Operator {
+    fn partial_cmp(&self, other: &Operator) -> Option<Ordering> {
+        Some(
+            self.priority().cmp(&other.priority())
+        )
     }
 }
 
@@ -103,5 +120,39 @@ mod tests {
             .collect();
 
         assert_eq!(result.is_empty(), true);
+    }
+
+    #[test]
+    fn operators_can_be_ordered_following_precedence_in_arithmetic() {
+        let result: Vec<bool> = [
+            (Operator::Caret > Operator::Times),
+            (Operator::Caret > Operator::Slash),
+            (Operator::Times > Operator::Plus),
+            (Operator::Times > Operator::Minus),
+            (Operator::Slash > Operator::Plus),
+            (Operator::Slash > Operator::Minus),
+
+            (Operator::Times < Operator::Caret),
+            (Operator::Slash < Operator::Caret),
+            (Operator::Plus < Operator::Times),
+            (Operator::Minus < Operator::Times),
+            (Operator::Plus < Operator::Slash),
+            (Operator::Minus < Operator::Slash),
+
+            (Operator::Times == Operator::Slash),
+            (Operator::Plus == Operator::Minus),
+            (Operator::Times > Operator::Slash),
+            (Operator::Slash < Operator::Times),
+            (Operator::Plus > Operator::Minus),
+            (Operator::Minus < Operator::Plus)
+        ].to_vec();
+
+        let expected_result: [bool; 18] = [
+            true, true, true, true, true, true,
+            true, true, true, true, true, true,
+            false, false, false, false, false, false
+        ];
+
+        assert_eq!(result, expected_result);
     }
 }
